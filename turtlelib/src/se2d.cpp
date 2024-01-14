8b6ceadfba590556862ba085f1b3529c09f90324
Rahul Roy <rahulroy2909@gmail.com>
2024-01-14 02:01:24 -0600

    finished implemeting all function...have to work on tests

--- turtlelib/src/se2d.cpp
+++ turtlelib/src/se2d.cpp
@@ -28,42 +28,53 @@ namespace turtlelib
         return is;
     }
 
-    Transform2D::Transform2D():trans{0.0,0.0}, rot(0.0){}
-    Transform2D::Transform2D(Vector2D trans):trans(trans),rot(0.0){}
-    Transform2D::Transform2D(double radians):trans{0.0,0.0},rot(radians){}
-    Transform2D::Transform2D(Vector2D trans, double radians):trans(trans),rot(radians){}
+    Transform2D::Transform2D():trans{0.0,0.0}, angle(0.0){}
+    Transform2D::Transform2D(Vector2D trans):trans(trans),angle(0.0){}
+    Transform2D::Transform2D(double radians):trans{0.0,0.0},angle(radians){}
+    Transform2D::Transform2D(Vector2D trans, double radians):trans(trans),angle(radians){}
 
     Point2D Transform2D::operator()(Point2D p) const
     {
         return {
-            cos(rot) * p.x - sin(rot) * p.y + trans.x,
-            sin(rot) * p.x + cos(rot) * p.y + trans.y
+            cos(angle) * p.x - sin(angle) * p.y + trans.x,
+            sin(angle) * p.x + cos(angle) * p.y + trans.y
         };
     }
 
     Vector2D Transform2D::operator()(Vector2D v) const
     {
         return {
-            cos(rot) * v.x - sin(rot) * v.y + trans.x,
-            sin(rot) * v.x + cos(rot) * v.y + trans.y
+            cos(angle) * v.x - sin(angle) * v.y + trans.x,
+            sin(angle) * v.x + cos(angle) * v.y + trans.y
         };
     }
     Twist2D Transform2D::operator()(Twist2D v) const
     {
 
         return{
-            v.omega, v.omega * trans.y + v.x * cos(rot) - v.y * sin(rot), -v.omega * trans.x + v.x * sin(rot) + v.y * cos(rot)
+            v.omega, v.omega * trans.y + v.x * cos(angle) - v.y * sin(angle), -v.omega * trans.x + v.x * sin(angle) + v.y * cos(angle)
 
         };
     }
 
     Transform2D Transform2D::inv() const
     {
-
+        Vector2D inv_xy = {-trans.x * cos(angle) - trans.y * sin(angle), -trans.y * cos(angle) + trans.x *sin(angle)};
+        double inv_rot = -angle;
+        return Transform2D{inv_xy, inv_rot};
     }
 
     Transform2D & Transform2D::operator*=(const Transform2D & rhs)
     {
+        double currentX = trans.x;
+        double currentY = trans.y;
+        double currentAngle = angle;
+
+        trans.x = cos(currentAngle) * rhs.trans.x - sin(currentAngle) * rhs.trans.y + currentX;
+        trans.y = sin(currentAngle) * rhs.trans.x + cos(currentAngle) * rhs.trans.y + currentY;
+
+        angle = currentAngle + rhs.angle;
+        return *this;
 
     }
     Vector2D Transform2D::translation() const
@@ -73,34 +84,34 @@ namespace turtlelib
 
     double Transform2D::rotation() const
     {
-        return rot;
+        return angle;
     }
 
     std::ostream & operator<<(std::ostream & os, const Transform2D & tf)
     {
-        return os << "deg" << rad2deg(tf.rot) << " " << "x" << tf.trans.x << " " << " y " << tf.trans.y;
+        return os << "deg" << rad2deg(tf.angle) << " " << "x" << tf.trans.x << " " << " y " << tf.trans.y;
     }
     
     std::istream & operator>>(std::istream & is, Transform2D & tf)
-    {   double rot = 0.0;
+    {   double angle = 0.0;
         Vector2D trans{0.0, 0.0};
         std::string s1,s2,s3;
         const auto c = is.peek();
         if ( c=='d')
         {  
             is >> s1;
-            is >> rot;
+            is >> angle;
             is >> s2;
             is >> trans.x;
             is >> s3; 
             is >> trans.y;   
         }
         else{
-        is >> rot >> trans.x >> trans.y;      
+        is >> angle >> trans.x >> trans.y;      
         }
         is.ignore(50, '\n');
-        rot = deg2rad(rot);
-        tf = Transform2D{trans,rot};
+        angle = deg2rad(angle);
+        tf = Transform2D{trans,angle};
         return is;
     }
 
