fc5f86a9ab68b4ecf8d21ead2f29b9883f0bf2fd
Rahul Roy <rahulroy2909@gmail.com>
2024-02-08 03:53:45 -0600

    format nusim

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,7 +1,7 @@
-// \file
-/// \brief Provides a simulated robot environment in RViz2.
-///        Displays walls, obstacles, and turtlebot.
-///
+/// \file
+/// \brief The nusim node provides a simulated robot environment.
+///        It uses Rviz2 for simulation. It is capable of creating
+///        stationary walls and tracking the position of a robot.
 ///
 /// PARAMETERS:
 ///     \param rate (int): Timer frequency (Hz)
@@ -16,54 +16,55 @@
 
 /// PUBLISHES:
 ///     \param ~/timestep (std_msgs::msg::UInt64): current timestep of the simulation
-///     \param ~/walls (visualization_msgs::msg::MarkerArray): MarkerArray of Walls in RViz2
-///     \param ~/obstacles (visualization_msgs::msg::MarkerArray): MarkerArray of cylindrical obstacles in RViz2
+///     \param ~/walls (visualization_msgs::msg::MarkerArray): MarkerArray of Walls in Rviz2
+///     \param ~/obstacles (visualization_msgs::msg::MarkerArray): MarkerArray of cylindrial obstacles in Rviz2
 /// SUBSCRIBES:
 ///     None
 /// SERVERS:
-///      \param ~/reset (std_srvs::srv::Empty): Resets the simulation
-///      \param ~/teleport (nusim::srv::Teleport): Teleports robot to a desired pose
+///      \param ~/reset (std_srvs::srv::Empty): Resets simulation
+///      \param ~/teleport (nusim::srv::Teleport): Teleports robot to a dessired position
 /// CLIENTS:
 ///     None
-
-#include <cstdio>
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
+#include <random>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp/logging.hpp"
+#include "std_msgs/msg/string.hpp"
 #include "std_msgs/msg/u_int64.hpp"
 #include "std_srvs/srv/empty.hpp"
-#include "tf2_ros/transform_broadcaster.h"
 #include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_broadcaster.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "nusim/srv/teleport.hpp"
 #include "visualization_msgs/msg/marker_array.hpp"
+#include "visualization_msgs/msg/marker.hpp"
 #include "nuturtlebot_msgs/msg/wheel_commands.hpp"
-#include "turtlelib/diff_drive.hpp"
 #include "nuturtlebot_msgs/msg/sensor_data.hpp"
+#include "turtlelib/diff_drive.hpp"
 
 using namespace std::chrono_literals;
 
-/// \brief The Nusim class provides a simulated environment for the robot
+/// \brief This class provides a simulator environment for the robot.
 ///        It publishes the current timestep of the simulation, along
-///        with an arena and cylindrical obstacles as markers in RViz2.
+///        with an arena and cylindrical obstacles as markers in Rviz2.
 ///        It offers services like reset and teleport.
 ///
 ///  \param rate (int): Timer frequency (Hz)
-///  \param x0 (double): Initial x-coordinate of the robot (m)
-///  \param y0 (double): Initial y-coordinate of the robot (m)
-///  \param theta0 (double): Initial theta angle of the robot (radians)
-///  \param x (double): Current x-coordinate of the robot (m)
-///  \param y (double): Current y-coordinate of the robot (m)
-///  \param theta (double): Current theta angle of the robot (radians)
-///  \param walls.arena_x_length (double): Length of arena in world x direction (m)
-///  \param walls.arena_y_length (double): Length of arena in world y direction (m)
-///  \param obstacles.x (std::vector<double>): List of the obstacles' x-coordinates (m)
-///  \param obstacles.y (std::vector<double>): List of the obstacles' y-coordinates (m)
-///  \param obstacles.r (double): Radius of cylindrical obstacles (m)
+///  \param x0_ (double): Initial x coordinate of the robot (m)
+///  \param y0_ (double): Initial y coordinate of the robot (m)
+///  \param theta0_ (double): Initial theta angle of the robot (radians)
+///  \param x_ (double): Current x coordinate of the robot (m)
+///  \param y_ (double): Current y coordinate of the robot (m)
+///  \param theta_ (double): Current theta angle of the robot (radians)
+///  \param arena_x_length_ (double): Length of arena in world x direction (m)
+///  \param arena_y_length_ (double): Length of arena in world y direction (m)
+///  \param obstacles_x_ (std::vector<double>): List of the obstacles' x coordinates (m)
+///  \param obstacles_y_ (std::vector<double>): List of the obstacles' y coordinates (m)
+///  \param obstacles_r_ (double): Radius of cylindrical obstacles (m)
 class Nusim : public rclcpp::Node
 {
 public:
@@ -103,19 +104,19 @@ public:
     obstacles_publisher_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(
       "~/obstacles", 10);
 
-    red_sensor_data_pub = create_publisher<nuturtlebot_msgs::msg::SensorData>("red/sensor_data", 10);
+    sensor_data_publisher_ = create_publisher<nuturtlebot_msgs::msg::SensorData>("red/sensor_data", 10);
 
 
     // Subscribers
-    red_wheel_sub = create_subscription<nuturtlebot_msgs::msg::WheelCommands>(
-      "red/wheel_cmd", 10, std::bind(&Nusim::red_wheel_callback, this, std::placeholders::_1));
+    wheel_cmd_subscriber_ = create_subscription<nuturtlebot_msgs::msg::WheelCommands>(
+      "red/wheel_cmd", 10, std::bind(&Nusim::wheel_cmd_callback, this, std::placeholders::_1));
 
     // Services
-    reset_service = this->create_service<std_srvs::srv::Empty>(
+    reset_ = this->create_service<std_srvs::srv::Empty>(
       "~/reset",
       std::bind(&Nusim::reset_callback, this, std::placeholders::_1, std::placeholders::_2));
 
-    teleport_service = this->create_service<nusim::srv::Teleport>(
+    teleport_ = this->create_service<nusim::srv::Teleport>(
       "~/teleport",
       std::bind(&Nusim::teleport_callback, this, std::placeholders::_1, std::placeholders::_2));
 
@@ -131,10 +132,42 @@ public:
     y_ = y0_;
     theta_ = theta0_;
 
-    wall_thickness_ = 0.1;
+    thickness_ = 0.10;
+    height_ = 0.25;
 
-    create_walls_();
-    create_obstacles();
+    // X coordinates of walls
+    x_pos_ = {
+      0.0,
+      0.0,
+      arena_x_length_ / 2 + thickness_ / 2,
+      -arena_x_length_ / 2 - thickness_ / 2,
+    };
+    // Y coordinates of walls
+    y_pos_ = {
+      arena_y_length_ / 2 + thickness_ / 2,
+      -arena_y_length_ / 2 - thickness_ / 2,
+      0.0,
+      0.0,
+    };
+    // X coordinate scaling of walls
+    x_scale_ = {
+      arena_x_length_ + 2.0 * thickness_,
+      arena_x_length_ + 2.0 * thickness_,
+      thickness_,
+      thickness_,
+    };
+    // y coordinate scaling of walls
+    y_scale_ = {
+      thickness_,
+      thickness_,
+      arena_y_length_,
+      arena_y_length_,
+    };
+    //
+    // Walls
+    wall_marker();
+    // Obstacles
+    obstacle_marker();
   }
 
 private:
@@ -169,8 +202,8 @@ private:
     // Send the transform
     tf_broadcaster_->sendTransform(t);
 
-    walls_publisher_->publish(wall_markers_array_);
-    obstacles_publisher_->publish(obstacles_markers_array_);
+    walls_publisher_->publish(wall_array_);
+    obstacles_publisher_->publish(obstacle_array_);
 
     update_robot_position();
     update_wheel_positions();
@@ -191,7 +224,7 @@ private:
     theta_ = robot_.get_config().theta;
 
 
-    RCLCPP_ERROR(this->get_logger(),"x = %f", x_);
+    // RCLCPP_ERROR(this->get_logger(),"x = %f", x_);
     // printf("Updated robot position: x = %f, y = %f, theta = %f\n", x_, y_, theta_);
 
 
@@ -211,7 +244,7 @@ private:
     sensor_data_msg_.stamp = get_clock()->now();
 
     // Publish on red/sensor_data
-    red_sensor_data_pub->publish(sensor_data_msg_);
+    sensor_data_publisher_->publish(sensor_data_msg_);
 
     // Reset previous wheel positions
     prev_wheel_pos_.left = updated_wheel_pos_.left;
@@ -219,7 +252,7 @@ private:
 
   }
 
-  void red_wheel_callback(const nuturtlebot_msgs::msg::WheelCommands & msg)
+  void wheel_cmd_callback(const nuturtlebot_msgs::msg::WheelCommands & msg)
   {
     // Left and right wheel velocity, in "motor command units" (mcu)
     // For the turtlebot, each motor can be command with an integer velocity of between
@@ -251,121 +284,56 @@ private:
     theta_ = request->theta;
   }
 
-  /// \brief Creates four walls around the arena
-  void create_walls_()
+  /// \brief Creates arena walls as a MarkerArray
+  void wall_marker()
   {
-    // The walls have a fixed height of 0.25m
-
-    visualization_msgs::msg::Marker wall_mark_1;
-    wall_mark_1.header.frame_id = "nusim/world";
-    wall_mark_1.header.stamp = get_clock()->now();
-    wall_mark_1.id = 1;
-    wall_mark_1.type = visualization_msgs::msg::Marker::CUBE;
-    wall_mark_1.action = visualization_msgs::msg::Marker::ADD;
-
-    wall_mark_1.pose.position.x = 0.0;
-    wall_mark_1.pose.position.y = arena_y_length_ / 2 + wall_thickness_ / 2;
-    wall_mark_1.pose.position.z = 0.25 / 2;
-
-    wall_mark_1.scale.x = arena_x_length_ + 2 * wall_thickness_;
-    wall_mark_1.scale.y = wall_thickness_;
-    wall_mark_1.scale.z = 0.25;
-
-    wall_mark_1.color.r = 1.0f;
-    wall_mark_1.color.a = 1.0f;
-
-    wall_markers_array_.markers.push_back(wall_mark_1);
-
-    visualization_msgs::msg::Marker wall_mark_2;
-    wall_mark_2.header.frame_id = "nusim/world";
-    wall_mark_2.header.stamp = get_clock()->now();
-    wall_mark_2.id = 2;
-    wall_mark_2.type = visualization_msgs::msg::Marker::CUBE;
-    wall_mark_2.action = visualization_msgs::msg::Marker::ADD;
-
-    wall_mark_2.pose.position.x = 0.0;
-    wall_mark_2.pose.position.y = -(arena_y_length_ / 2 + wall_thickness_ / 2);
-    wall_mark_2.pose.position.z = 0.25 / 2;
-
-    wall_mark_2.scale.x = arena_x_length_ + 2 * wall_thickness_;
-    wall_mark_2.scale.y = wall_thickness_;
-    wall_mark_2.scale.z = 0.25;
-
-    wall_mark_2.color.r = 1.0f;
-    wall_mark_2.color.a = 1.0f;
-
-    wall_markers_array_.markers.push_back(wall_mark_2);
-
-    visualization_msgs::msg::Marker wall_mark_3;
-    wall_mark_3.header.frame_id = "nusim/world";
-    wall_mark_3.header.stamp = get_clock()->now();
-    wall_mark_3.id = 3;
-    wall_mark_3.type = visualization_msgs::msg::Marker::CUBE;
-    wall_mark_3.action = visualization_msgs::msg::Marker::ADD;
-
-    wall_mark_3.pose.position.x = arena_x_length_ / 2 + wall_thickness_ / 2;
-    wall_mark_3.pose.position.y = 0.0;
-    wall_mark_3.pose.position.z = 0.25 / 2;
-
-    wall_mark_3.scale.x = wall_thickness_;
-    wall_mark_3.scale.y = arena_y_length_ + 2 * wall_thickness_;
-    wall_mark_3.scale.z = 0.25;
-
-    wall_mark_3.color.r = 1.0f;
-    wall_mark_3.color.a = 1.0f;
-
-    wall_markers_array_.markers.push_back(wall_mark_3);
-
-    visualization_msgs::msg::Marker wall_mark_4;
-    wall_mark_4.header.frame_id = "nusim/world";
-    wall_mark_4.header.stamp = get_clock()->now();
-    wall_mark_4.id = 4;
-    wall_mark_4.type = visualization_msgs::msg::Marker::CUBE;
-    wall_mark_4.action = visualization_msgs::msg::Marker::ADD;
-
-    wall_mark_4.pose.position.x = -(arena_x_length_ / 2 + wall_thickness_ / 2);
-    wall_mark_4.pose.position.y = 0.0;
-    wall_mark_4.pose.position.z = 0.25 / 2;
-
-    wall_mark_4.scale.x = wall_thickness_;
-    wall_mark_4.scale.y = arena_y_length_ + 2 * wall_thickness_;
-    wall_mark_4.scale.z = 0.25;
-
-    wall_mark_4.color.r = 1.0f;
-    wall_mark_4.color.a = 1.0f;
-
-    wall_markers_array_.markers.push_back(wall_mark_4);
+    for (int i = 0; i < 4; i++) {
+      visualization_msgs::msg::Marker walls;
+      walls.header.frame_id = "nusim/world";
+      walls.header.stamp = get_clock()->now();
+      walls.id = i;
+      walls.type = visualization_msgs::msg::Marker::CUBE;
+      walls.action = visualization_msgs::msg::Marker::ADD;
+      walls.pose.position.x = x_pos_.at(i);
+      walls.pose.position.y = y_pos_.at(i);
+      walls.scale.x = x_scale_.at(i);
+      walls.scale.y = y_scale_.at(i);
+      walls.color.r = 1.0;
+      walls.color.g = 0.0;
+      walls.color.b = 0.0;
+      walls.color.a = 1.0;
+      walls.pose.position.z = 0.0;
+      walls.scale.z = 0.25;
+      wall_array_.markers.push_back(walls);
+    }
   }
 
-  /// \brief Creates static cylindrical obstacles in the simulation
-  void create_obstacles()
+  /// \brief Creates cylindrical obstacles as a MarkerArray
+  void obstacle_marker()
   {
     if (obstacles_x_.size() != obstacles_y_.size()) {
-      RCLCPP_ERROR(
-        this->get_logger(), "Error: obstacles/x and obstacles/y should be the same length.");
+      RCLCPP_ERROR(this->get_logger(), "Lengths of obstacles/x and obstacles/y must be the same.");
       rclcpp::shutdown();
+      return;
     }
-    const auto num_markers = obstacles_x_.size();
-
-    for (long unsigned int i = 0; i < num_markers; i++) {
+    for (size_t i = 0; i < obstacles_x_.size(); i++) {
       visualization_msgs::msg::Marker obs;
       obs.header.frame_id = "nusim/world";
       obs.header.stamp = get_clock()->now();
       obs.id = i;
       obs.type = visualization_msgs::msg::Marker::CYLINDER;
       obs.action = visualization_msgs::msg::Marker::ADD;
-
       obs.pose.position.x = obstacles_x_.at(i);
       obs.pose.position.y = obstacles_y_.at(i);
-      obs.pose.position.z = 0.125;
-
+      obs.pose.position.z = 0.0;
       obs.scale.x = 2.0 * obstacles_r_;
       obs.scale.y = 2.0 * obstacles_r_;
       obs.scale.z = 0.25;
-
       obs.color.r = 1.0;
+      obs.color.g = 0.0;
+      obs.color.b = 0.0;
       obs.color.a = 1.0;
-      obstacles_markers_array_.markers.push_back(obs);
+      obstacle_array_.markers.push_back(obs);
     }
   }
 
@@ -375,19 +343,19 @@ private:
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr walls_publisher_;
   rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr obstacles_publisher_;
 
-  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_service;
-  rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_service;
+  rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_;
+  rclcpp::Service<nusim::srv::Teleport>::SharedPtr teleport_;
 
   std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
-  visualization_msgs::msg::MarkerArray wall_markers_array_;
-  visualization_msgs::msg::MarkerArray obstacles_markers_array_;
+  visualization_msgs::msg::MarkerArray wall_array_;
+  visualization_msgs::msg::MarkerArray obstacle_array_;
 
   // Publishers
-  rclcpp::Publisher<nuturtlebot_msgs::msg::SensorData>::SharedPtr red_sensor_data_pub;
+  rclcpp::Publisher<nuturtlebot_msgs::msg::SensorData>::SharedPtr sensor_data_publisher_;
 
   // Subscribers
-  rclcpp::Subscription<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr red_wheel_sub;
+  rclcpp::Subscription<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr wheel_cmd_subscriber_;
 
   int timestep_;
   int rate_;
@@ -397,9 +365,10 @@ private:
   double x_;
   double y_;
   double theta_;
+  double height_;
   double arena_x_length_;
   double arena_y_length_;
-  double wall_thickness_;
+  double thickness_;
   std::vector<double> obstacles_x_;
   std::vector<double> obstacles_y_;
   double obstacles_r_;
@@ -410,6 +379,8 @@ private:
   turtlelib::WheelPos prev_wheel_pos_{0.0, 0.0};
   nuturtlebot_msgs::msg::SensorData sensor_data_msg_;
   turtlelib::DiffDrive robot_;
+  std::vector<double> x_pos_, y_pos_, x_scale_, y_scale_;
+  
   
 
 };
