241e11dd0b9c718ca3c276f20dd6a5c987be017c
Rahul Roy <rahulroy2909@gmail.com>
2024-03-11 05:07:27 -0500

    fixed circle-FIRTTING

--- turtlelib/src/circle_fit.cpp
+++ turtlelib/src/circle_fit.cpp
@@ -22,36 +22,24 @@ namespace turtlelib
         x_hat /= num;
         y_hat /= num;
 
-        //shift coordinates so that the centroid is at the origin
-        std::vector<Vector2D> shifted_points;
-        for (const auto &p : points)
+        // Shift coordinates so that the centroid is at the origin and compute z_i = x_i^2 + y_i^2
+        double n = static_cast<double>(points.size());
+        arma::mat Z(points.size(), 4, arma::fill::zeros);
+        arma::vec sum_z(points.size(), arma::fill::zeros);
+
+        for (size_t i = 0; i < points.size(); i++)
         {
-            shifted_points.push_back(p - Vector2D{x_hat, y_hat});
+            Z(i, 1) = points[i].x - x_hat;
+            Z(i, 2) = points[i].y - y_hat;
+            Z(i, 0) = Z(i, 1) * Z(i, 1) + Z(i, 2) * Z(i, 2);
+            Z(i, 3) = 1;
+
+            sum_z(i) = Z(i, 0);
         }
 
-        //compute z_i= x_i^2 + y_i^2
-        std::vector<double> z_i;
-        for (const auto &p : shifted_points)
-        {
-            z_i.push_back(p.x * p.x + p.y * p.y);
-        }
-        //compute mean of z
-        auto z_hat = 0.0;
-        double n = points.size();
-        for (const auto &z : z_i)
-        {
-            z_hat += z;
-        }
-        z_hat /= n;
-        // form data matrix from n data points
-        arma::mat Z{n, 4};
-        for (size_t i = 0; i < n; i++)
-        {
-            Z(i, 0) = z_i.at(i);
-            Z(i, 1) = shifted_points[i].x;
-            Z(i, 2) = shifted_points[i].y;
-            Z(i, 3) = 1;
-        }
+        // Compute mean of z
+            auto z_hat = arma::mean(sum_z);
+        
         //form the momemt matrix , M=1/n*Z^T*Z
         arma::mat M = (1 / n) * Z.t() * Z;
 
@@ -62,16 +50,14 @@ namespace turtlelib
         H(0,3) = 2.0;
         H(1,1) = 1.0;
         H(2,2) = 1.0;
-        H(1,1) = 1.0;
-        H(2,2) = 1.0;
-
+        
         //compute H inverse
         arma::mat H_inv = arma::mat(4, 4, arma::fill::zeros);
         H_inv(3,0) = 0.5;
         H_inv(0,3) = 0.5;
         H_inv(1,1) = 1.0;
         H_inv(2,2) = 1.0;
-        H_inv(4,4) = -2.0*z_hat;
+        H_inv(3,3) = -2.0*z_hat;
 
         //compute singular value decomposition of Z
         arma::mat U;
@@ -94,16 +80,26 @@ namespace turtlelib
             arma::vec eigen_values;
             arma::mat eigen_vectors;
             arma::eig_sym(eigen_values, eigen_vectors, Q);
-            auto min_Index = arma::index_min(arma::real(eigen_values.elem(arma::find(arma::real(eigen_values) > 0))));
-            arma::vec A_star = eigen_vectors.col(min_Index);
-            A = V*A_star;
+            auto min = 1e12;
+            size_t min_index = 0;
+            for (size_t i = 0; i < arma::real(eigen_values).size(); i++)
+            {
+                if (arma::real(eigen_values)(i) < min && arma::real(eigen_values)(i) > 0.0) 
+                {
+                    min = arma::real(eigen_values)(i);
+                    min_index = i;
+                }
+            }
+            
+            arma::vec A_star = arma::real(eigen_vectors).col(min_index);
+            A = Y.i()*A_star;
 
 
         }
         //compute the circle equation
-        double a=-A(1)/(2.0*A(0));
-        double b=-A(2)/(2.0*A(0));
-        double r=std::sqrt(A(1)*A(1)+A(2)*A(2)-4*A(0)*A(3))/(2*std::abs(A(0)));
+        double a=-A.at(1)/(2.0*A.at(0));
+        double b=-A.at(2)/(2.0*A.at(0));
+        double r=std::sqrt(A.at(1)*A.at(1)+A.at(2)*A.at(2)-4*A.at(0)*A.at(3))/(2*std::abs(A.at(0)));
         //shift the circle back to the original coordinates
         double circle_x = a + x_hat;
         double circle_y = b + y_hat;
